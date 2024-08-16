973f91ecc9ed7745c4dfcb8b40e0edecd5af2ba3
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2024-08-16 10:47:30 +0530

    Add files via upload

---
+++ AuthenticationController.cs
@@ -0,0 +1,47 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WebAPI.Model;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        ILogin login;
+        public AuthenticationController(ILogin _login)
+        {
+            login = _login; 
+        }
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] Login user)
+        {
+
+            if (user is null)
+            {
+                return BadRequest("Invalid user request!!!");
+            }
+
+            if (login.ValidateUser(user.UserName,user.Password))
+            {
+                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(ConfigurationManager.AppSetting["JWT:Secret"]));
+                var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+                var tokeOptions = new JwtSecurityToken(
+                    issuer: ConfigurationManager.AppSetting["JWT:ValidIssuer"],
+                    audience: ConfigurationManager.AppSetting["JWT:ValidAudience"],
+                    claims: new List<Claim>(),
+                    expires: DateTime.Now.AddMinutes(6),
+                    signingCredentials: signinCredentials
+                );
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+                return Ok(new JWTTokenResponse { Token = tokenString });
+            }
+            return Unauthorized();
+        }
+    }
+}
---
+++ ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Serialization;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+       
+        public ContentResult Get() {
+
+         return Content("You are a authorised user");
+
+        }
+
+
+    }
+}
