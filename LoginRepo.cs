92251824af87ab469cfca3712d64b35f7f12dd33
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2024-08-16 10:46:56 +0530

    Add files via upload

---
+++ LoginRepo.cs
@@ -0,0 +1,28 @@
+﻿namespace WebAPI.Models
+{
+    public interface ILogin
+    {
+        bool ValidateUser(string uname,string pwd);
+    }
+    public class LoginRepo : ILogin
+    {
+        NorthwindContext context;
+        public LoginRepo(NorthwindContext _context)
+        {
+            context = _context;     
+        }
+        public bool ValidateUser(string uname,string pwd)
+        {
+            User user=context.Users.Find(uname);
+            if (user.Password == pwd)
+            {
+                return true;    
+            }
+            else
+            {
+                return  false;  
+            }
+            
+        }
+    }
+}
